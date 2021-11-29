8586505681c1915ff4b296d713106beff26db767
hak0_hand <sonson112828@gmail.com>
2021-11-29 23:41:34 +0900

    1129_hy_home,write,output화면및서버연동

--- 손학영/pixbygan.py
+++ 손학영/pixbygan.py
@@ -1,9 +1,10 @@
 '''모듈 불러오기'''
 
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, File, UploadFile, Form
 from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+import requests
 
 import uvicorn
 import os
@@ -12,26 +13,61 @@ import os
 app = FastAPI()
 
 # src 연결
-app.mount("/static", StaticFiles(directory="static"), name="static") # 첨부 이미지
+app.mount("/static", StaticFiles(directory="static"), name="static") # css 파일
 templates = Jinja2Templates(directory = "templates") # html 파일
 
 # root 연결
-@app.get('/', response_class= HTMLResponse)
-async def home(request : Request) :
-    # base_url = '../img_data/'
-    # path = os.listdir('./img_data')[:8]
-    # img1 = base_url + path[0]
-    # img2 = base_url + path[1]
-    # img3 = base_url + path[2]
-    # img4 = base_url + path[3]
-    # img5 = base_url + path[4]
-    # img6 = base_url + path[5]
-    # img7 = base_url + path[6]
-    # img8 = base_url + path[7]
-    # return templates.TemplateResponse("home.html", context={"request": request, "img1" : img1, "img2" : img2, "img3" : img3,
-    #                                                         "img4" : img4, "img5" : img5, "img6" : img6,"img7" : img7,
-    #                                                         "img8" : img8})
+@app.get('/')
+async def root() :
+    return {"hello" : "world"}
 
+
+@app.get('/home', response_class = HTMLResponse)
+async def home(request : Request) :
     return templates.TemplateResponse("home.html", context={"request": request})
 
+@app.get('/home_img/{img_num}')
+async def home_img(img_num : int) :
+    home_img = os.listdir("./static/output")
+    img_src = "./static/output/" + home_img[img_num]
+    return FileResponse(img_src)
+
+
+
+@app.get('/write', response_class = HTMLResponse)
+async def write(request : Request) :
+
+    return templates.TemplateResponse("write.html", context={"request": request})
+
+
+@app.post("/runmodel/", response_class = HTMLResponse)
+async def runmodel(request : Request, files: UploadFile = File(...)):
+    global file_location
+    file_location = f"./static/input/{files.filename}"
+    with open(file_location, "wb+") as file_object:
+        file_object.write(files.file.read())
+
+    # file_path = './output/covid19.csv'
+    return templates.TemplateResponse("output.html", context={"request": request})
+
+
+
+@app.get('/output', response_class = HTMLResponse)
+async def output(request : Request) :
+
+    return templates.TemplateResponse("output.html", context={"request": request})
+
+@app.get('/output_img')
+async def home_img() :
+    output_src = file_location
+    return FileResponse(output_src)
+
+
+@app.get('/download')
+async def download() :
+    file_path = './static/output/20211119_101611_12.png'
+    return FileResponse(path = file_location, filename='character.png')
+
+
 uvicorn.run(app, port=8000)
+
